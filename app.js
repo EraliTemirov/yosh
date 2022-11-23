31f75c2a026b8a7a058438896f653aea14fb4701
Temirov Erali <eralitemirov129@gmail.com>
2022-11-23 22:34:20 +0500

    qwe

---
+++ app.js
@@ -0,0 +1,14 @@
+
+//console.log(now/1000/60/60/24/365.25);
+//console.log((now-maxsus)/1000/60/60/24/365.25);
+
+
+
+
+var sana = prompt("Tug'ilgan sanangizni kiriting");
+let san = new Date("sana");
+let now = Date.now();
+alert("Sizning yoshingiz: " + (now - san));
+
+
+
