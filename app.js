9fc951e94355ec2de51f163d33de62878e48e7e8
Temirov Erali <eralitemirov129@gmail.com>
2022-11-24 11:16:58 +0500

    qwerty

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-
-//console.log(now/1000/60/60/24/365.25);
-//console.log((now-maxsus)/1000/60/60/24/365.25);
-
-
-
-
 var sana = prompt("Tug'ilgan sanangizni kiriting");
-let san = new Date("sana");
-let now = Date.now();
-alert("Sizning yoshingiz: " + (now - san));
 
-
-
+let vaqt = new Date(sana).getTime();
+let now = new Date().getTime();
+alert(
+  "Sizning yoshingiz: " +
+    Math.floor((now - vaqt) / 1000 / 3600 / 24 / 365.25) +
+    "\n" +
+    "kun:" +
+    Math.floor(
+      ((now - vaqt) / 1000 / 3600 / 24 / 365.25 -
+        Math.floor((now - vaqt) / 1000 / 3600 / 24 / 365.25)) *
+        365.25
+    ) +
+    "\n" +
+    "soat:" +
+    Math.floor(
+      (((now - vaqt) / 1000 / 3600 / 24 / 365.25 -
+        Math.floor((now - vaqt) / 1000 / 3600 / 24 / 365.25)) *
+        365.25 -
+        Math.floor(
+          ((now - vaqt) / 1000 / 3600 / 24 / 365.25 -
+            Math.floor((now - vaqt) / 1000 / 3600 / 24 / 365.25)) *
+            365.25
+        )) *
+        24
+    )
+);
